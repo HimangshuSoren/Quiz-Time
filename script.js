4df0d62d062f4eb58a9513f27602ceaedc883199
Himangshu <67628306+HimangshuSoren@user.noreplay.github.com>
2024-09-22 22:30:09 +0530

    main-page-logo-added

--- script.js
+++ script.js
@@ -5,15 +5,27 @@ setTimeout(()=>{
     document.body.classList.remove('overflow-hide')
 },5500)
 
+
+//---->[Start Button Event Listener]
 startButton.addEventListener('click',()=>{
     startButton.classList.add('black-hole')
     setTimeout(()=>{
         document.querySelector('.logo-2').classList.add('black-hole')
         clearInterval(dance)
     },400)
+    setTimeout(()=>{
+
+        document.querySelector('.main-game').classList.add('start')
+    },800)
+    setTimeout(()=>{
+        document.querySelector('#logo-in-game').classList.add('pop-animation-logo-in-game')
+    },1700)
+
 
 })
 
+
+//---->[Dancing Logo]
 const dance = setInterval(()=>{
         document.querySelector('.l-2-container').classList.toggle('left-side')
 },400)
